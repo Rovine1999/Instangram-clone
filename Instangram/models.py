5e4ce40d26f59bd8a5a346b3983b4330dc61a661
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-17 07:39:17 +0300

    add models for profile, image and comment for user profile, image and comment

--- Instangram/models.py
+++ Instangram/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+import cloudinary
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Profile(models.Model):
+    # form.instance.user = Profile.objects.get(user=self.request.user)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = CloudinaryField('image')
+    
+    
+
+
+class Image(models.Model):
+    image = CloudinaryField('image')
+    image_name = models.CharField(max_length=255)
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    author_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, null=True)
+    likes = models.ManyToManyField(User, related_name = 'likes', blank = True)
+    
+    
+    
+class Comment(models.Model):
+    comment = models.TextField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE )
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    
