6e9ff3b6c1514e57bee7ced2393215ed7f284ff3
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-17 07:42:41 +0300

    methods for the user profile

--- Instangram/models.py
+++ Instangram/models.py
@@ -13,7 +13,30 @@ class Profile(models.Model):
     bio = models.TextField(blank=True)
     photo = CloudinaryField('image')
     
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+        
     
+    @classmethod
+    def get_profile(request, id):
+        try:
+            profile = Profile.objects.get(pk = id)
+            print(image)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return profile
+        
+    @classmethod
+    def update_profile(cls, id, value):
+        cls.objects.filter(id=id).update(name = value)
+        
+    def __str__(self):
+        return self.bio 
 
 
 class Image(models.Model):
